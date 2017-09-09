3fd8911b8425b1370eb523e045e0d07eb73a8b42
humeniuk-vs <vg6962@gmail.com>
2017-09-09 19:49:19 +0300

    jss implement

--- src/scripts/test.js
+++ src/scripts/test.js
@@ -4,7 +4,9 @@
   let blogTitle = document.querySelector('.blog-title');
   let titleText = document.createTextNode('Mister Mercedes');
 
-  blogTitle.appendChild(titleText);
+  setTimeout(function() {
+    blogTitle.appendChild(titleText);
+  }, 1330);
 
   let blogText = document.querySelector('.blog-text');
   let textText = document.createTextNode('Many jobless people are standing in line for a job fair when a Mercedes plows into the crowd killing 8 people and severely injuring many. Bill Hodges, a recently retired detective from the local police department living the life of a retiree, receives a letter from an individual claiming to be the person responsible for the job fair incident, referring to himself as "Mr.Mercedes".');
--- src/scripts/untilload.js
+++ src/scripts/untilload.js
@@ -1,3 +1,6 @@
+import 'jss';
+import { StyleSheet, css } from 'aphrodite-jss';
+
 ((() => {
   let untilLoad = document.querySelectorAll('[data-attribute="untilLoad"]');
   untilLoad = Array.prototype.slice.call(untilLoad);
@@ -5,10 +8,10 @@
   let inlineChunk = [];
   let blockChunk = [];
 
-  untilLoad.forEach(el => {
-    let untilLoadChildren = Array.prototype.slice.call(el.children, 0);
+  untilLoad.forEach((el) => {
+    let untilLoadChildren = untilLoad.slice.call(el.children, 0);
 
-    untilLoadChildren.forEach(el => {
+    untilLoadChildren.forEach((el) => {
       let computedType = window.getComputedStyle(el).display.toString();
 
       if (computedType === 'block') {
@@ -19,5 +22,23 @@
     });
   });
 
-  console.log(blockChunk, inlineChunk);
+  blockChunk.forEach((el) => {
+    let placeholderHeigth = window.getComputedStyle(el).lineHeight;
+
+    const sheet = StyleSheet.create({
+      placeholder: {
+        backgroundColor: 'currentColor',
+        opacity: '.2',
+        width: '100%',
+        height: placeholderHeigth,
+        fontSize: 33
+      }
+    });
+
+    let placeholder = `<div class="${css(sheet.placeholder)}"></div>`;
+
+    el.innerHTML += placeholder;
+  });
+
+  // console.log(blockChunk, inlineChunk);
 }))();
