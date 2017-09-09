94909a1f25c5ee36143504ea7334be593cfc2b95
humeniuk-vs <vg6962@gmail.com>
2017-09-10 00:53:48 +0300

    image ident method

--- src/scripts/test.js
+++ src/scripts/test.js
@@ -6,19 +6,19 @@
 
   setTimeout(function() {
     blogTitle.appendChild(titleText);
-  }, 1330);
+  }, 1000);
 
   let blogText = document.querySelector('.blog-text');
   let textText = document.createTextNode('Many jobless people are standing in line for a job fair when a Mercedes plows into the crowd killing 8 people and severely injuring many. Bill Hodges, a recently retired detective from the local police department living the life of a retiree, receives a letter from an individual claiming to be the person responsible for the job fair incident, referring to himself as "Mr.Mercedes".');
 
   setTimeout(function() {
     blogText.appendChild(textText);
-  }, 1120);
+  }, 1000);
 
   let blogDate = document.querySelector('.blog-date');
   let dateText = document.createTextNode('23 december 2017');
 
   setTimeout(function() {
     blogDate.appendChild(dateText);
-  }, 3330);
+  }, 1000);
 })();
--- src/scripts/untilload.js
+++ src/scripts/untilload.js
@@ -5,37 +5,49 @@ import { StyleSheet, css } from 'aphrodite-jss';
   let untilLoad = document.querySelectorAll('[data-attribute="untilLoad"]');
   untilLoad = Array.prototype.slice.call(untilLoad);
 
-  let inlineChunk = [];
-  let blockChunk = [];
-
   untilLoad.forEach((el) => {
     let untilLoadChildren = untilLoad.slice.call(el.children, 0);
 
     untilLoadChildren.forEach((el) => {
-      let placeholderHeigth = window.getComputedStyle(el).lineHeight;
+      const placeholderHeigth = window.getComputedStyle(el).lineHeight;
+      const imageWidth = window.getComputedStyle(el).maxWidth.toString();
+      const imageHeight = window.getComputedStyle(el).maxHeight.toString();
+      const imageMinHeight = el.offsetHeight;
 
       const sheet = StyleSheet.create({
         placeholder: {
           '&:empty': {
             backgroundColor: 'currentColor',
-            opacity: '.2',
+            filter: 'brightness(380%)',
             overflow: 'hidden',
             width: 'auto',
             height: placeholderHeigth
           }
+        },
+        placeholderImage: {
+          display: 'inline-block',
+          filter: 'brightness(380%)',
+          backgroundColor: 'currentColor',
+          width: imageWidth,
+          height: imageHeight === 'none' ? 'auto' : imageHeight,
+          minHeight: imageMinHeight
+        },
+        hidden: {
+          display: 'none'
         }
       });
 
-      let computedType = window.getComputedStyle(el).display.toString();
+      const imagePlaceholder = document.createElement('div');
+      imagePlaceholder.classList.add(`${css(sheet.placeholderImage)}`);
 
-      if (computedType === 'block') {
-        blockChunk.push(el);
-        el.classList.add(`${css(sheet.placeholder)}`);
+      const image = el.hasAttribute('src');
+
+      if (image) {
+        el.classList.add(`${css(sheet.hidden)}`);
+        el.parentNode.insertBefore(imagePlaceholder, el);
       } else {
-        inlineChunk.push(el);
+        el.classList.add(`${css(sheet.placeholder)}`);
       }
     });
   });
-
-  console.log(blockChunk, inlineChunk);
 }))();
