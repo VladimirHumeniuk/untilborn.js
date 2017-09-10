86926821748520b1f83bb8960b6ddf7393d8aca8
humeniuk-vs <vg6962@gmail.com>
2017-09-10 11:06:10 +0300

    lint hotfixes

--- src/scripts/test.js
+++ src/scripts/test.js
@@ -5,23 +5,23 @@
     xhr.responseType = 'json';
     xhr.onload = () => {
       const status = xhr.status;
-      if (status == 200) {
+      if (status === 200) {
         callback(null, xhr.response);
       } else {
         callback(status);
       }
     };
     xhr.send();
-};
+  };
 
-    const blogTitle = document.querySelector('.blog-title');
-    const blogText = document.querySelector('.blog-text');
-    const blogDate = document.querySelector('.blog-date');
-    const blogImage = document.querySelector('.blog-image');
+  const blogTitle = document.querySelector('.blog-title');
+  const blogText = document.querySelector('.blog-text');
+  const blogDate = document.querySelector('.blog-date');
+  const blogImage = document.querySelector('.blog-image');
 
-    getJSON('/test-data.json', (err, data) => {
+  getJSON('/test-data.json', (err, data) => {
     if (err != null) {
-      alert(err);
+      return false;
     } else {
       blogImage.src = data.image;
       blogTitle.innerText = data.title;
