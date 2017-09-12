b42dc394e54834acc3f2fca6cafc990322eed70b
humeniuk-vs <vg6962@gmail.com>
2017-09-12 12:10:49 +0300

    in progress

--- src/scripts/untilload.js
+++ src/scripts/untilload.js
@@ -9,36 +9,50 @@ import { StyleSheet, css } from 'aphrodite-jss';
     let untilLoadChildren = untilLoad.slice.call(el.children, 0);
 
     untilLoadChildren.forEach((el) => {
+      const placeholderWidth = window.getComputedStyle(el).width;
+      const placeholderMaxWidth = window.getComputedStyle(el).maxWidth;
+      const placeholderMargin = window.getComputedStyle(el).marginBottom;
       const placeholderHeigth = window.getComputedStyle(el).lineHeight;
-      const imageWidth = window.getComputedStyle(el).maxWidth.toString();
-      const imageHeight = window.getComputedStyle(el).maxHeight.toString();
-      const imageMinHeight = el.offsetHeight;
+
+      const imageMargin = window.getComputedStyle(el).fontSize;
 
       const sheet = StyleSheet.create({
         placeholder: {
-          '&:empty': {
-            backgroundColor: 'currentColor',
-            filter: 'brightness(380%)',
-            overflow: 'hidden',
-            width: 'auto',
-            height: placeholderHeigth
-          }
+          backgroundColor: 'currentColor',
+          opacity: .6,
+          width: placeholderWidth === 'none' ? 'auto' : placeholderWidth,
+          maxWidth: placeholderMaxWidth === 'none' ? '100%' : placeholderMaxWidth,
+          height: placeholderHeigth,
+          marginBottom: placeholderMargin
         },
         placeholderImage: {
-          display: 'inline-block',
           backgroundColor: 'currentColor',
-          width: imageWidth,
-          height: imageHeight === 'none' ? 'auto' : imageHeight,
-          minHeight: imageMinHeight
+          marginBottom: imageMargin,
+          opacity: .6
+        },
+        untilLoad: {
+          display: 'none'
         }
+
       });
 
+      HTMLElement.prototype.wrap = function(wrapper){
+        this.parentNode.insertBefore(wrapper, this);
+        wrapper.appendChild(this);
+      }
+
       const image = el.hasAttribute('src');
+      const placeholder = document.createElement('div');
+      placeholder.className = `${css(sheet.placeholder)}`;
+
+      const placeholderImage = document.createElement('div');
+      placeholderImage.className = `${css(sheet.placeholderImage)}`;
 
       if (image) {
-        el.classList.add(`${css(sheet.placeholderImage)}`);
+        el.wrap(placeholderImage);
       } else {
-        el.classList.add(`${css(sheet.placeholder)}`);
+        el.classList.add(`${css(sheet.untilLoad)}`);
+        el.parentNode.insertBefore(placeholder, el);
       }
     });
   });
